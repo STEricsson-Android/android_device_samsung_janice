9594f0f6471dea5bf1f6cd747a0e4e855d8fd91c
Alberto Pedron <albertopedron1997@yahoo.it>
2013-08-10 21:46:57 +0200

    Disabled cyanogenmod + bootanimation
    ### --> Edited lines

--- cm.mk
+++ cm.mk
@@ -9,12 +9,12 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+### $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # If the boot animation is not automatically selected, we chose it here :)
 
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
+### PRODUCT_COPY_FILES +=  \
+###    vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
 
 
 # Inherit device configuration
@@ -22,7 +22,7 @@ $(call inherit-product, device/samsung/janice/full_janice.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := janice
-PRODUCT_NAME := cm_janice
+PRODUCT_NAME := janice
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9070
 PRODUCT_MANUFACTURER := samsung
